ed210e87c7c5c4e250a712eefeab101bd23cc800
Nishanth Shetty <129318811+shettynishanth@users.noreply.github.com>
2024-09-22 10:28:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
               <ImageSlider />
               <PleasedDisplay />
               <KeyFeaturesSection />
-              <LectureProfiles />
+              // <LectureProfiles />
               <StudentOpinions />
               <YouTubeChannelPromo />
               <CounsellingSection />
