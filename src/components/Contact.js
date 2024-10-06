3913a7c5287442868e4beaf49b19bed18cc26ed4
Nishanth Shetty <129318811+shettynishanth@users.noreply.github.com>
2024-10-06 11:04:47 +0530

    Update Contact.js
    update ifrane size

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -44,20 +44,21 @@ const ContactPage = () => {
       </div>
 
       {/* Google Map Section */}
-      <div className="mt-12 px-36">
-        <h3 className="text-3xl font-bold mb-8 text-center text-gray-900">
-          Our Location
-        </h3>
-        <div className="aspect-w-16 aspect-h-10 rounded-lg shadow-lg overflow-hidden">
-          <iframe
-            className="w-full h-full "
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46231.81990080575!2d74.65994064280866!3d13.631636370379319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbc8f0048daefe5%3A0x5b9b0c7cc8071387!2sSpardha%20Sarathi%20KAS%20Academy%20Kundapura!5e0!3m2!1sen!2sin!4v1726911075283!5m2!1sen!2sin"
-            allowFullScreen=""
-            loading="lazy"
-            title="Spardha Sarathi KAS Academy Location"
-          ></iframe>
-        </div>
-      </div>
+      <div className="mt-12 px-4 sm:px-8 md:px-16 lg:px-36 xl:px-48">
+  <h3 className="text-2xl sm:text-3xl font-bold mb-8 text-center text-gray-900">
+    Our Location
+  </h3>
+  <div className="aspect-w-16 aspect-h-10 rounded-lg shadow-lg overflow-hidden">
+    <iframe
+      className="w-full h-full"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46231.81990080575!2d74.65994064280866!3d13.631636370379319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbc8f0048daefe5%3A0x5b9b0c7cc8071387!2sSpardha%20Sarathi%20KAS%20Academy%20Kundapura!5e0!3m2!1sen!2sin!4v1726911075283!5m2!1sen!2sin"
+      allowFullScreen=""
+      loading="lazy"
+      title="Spardha Sarathi KAS Academy Location"
+    ></iframe>
+  </div>
+</div>
+
     </div>
   );
 };
